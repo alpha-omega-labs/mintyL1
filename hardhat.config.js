07e45ed50c098ce2892ddef58677e905edba5764
alpha-omega-labs <88520218+alpha-omega-labs@users.noreply.github.com>
2022-07-17 21:29:59 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,13 +4,15 @@ require("@nomiclabs/hardhat-waffle");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: "0.7.0",
+    solidity: "0.7.3",
 
     defaultNetwork: 'localhost',
     networks: {
         hardhat: {},
         localhost: {
       url: "http://23.88.68.53:8545",
+      gasPrice: 2000000000,
+      chainId: 29,
       accounts: []},
     }
 };
